45567609f7ca143fa4e3d6a7270420e0164f48cd
sanjblro <kunfaepoh22@gmail.com>
2025-11-20 20:14:25 +0700

    0.1

---
+++ Assets/Script/SceneChange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  // ต้องใช้ namespace นี้สำหรับการจัดการฉาก
+
+public class SceneChange : MonoBehaviour
+{
+    // ฟังก์ชันที่จะถูกเรียกเมื่อกดปุ่ม
+    public void ChangeScene(string sceneName)
+    {
+        // โหลดฉากตามชื่อที่ระบุ
+        SceneManager.LoadScene(sceneName);
+    }
+}
